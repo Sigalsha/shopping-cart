3ea7cbce1ee49ce24888f400adaafca195fef9cb
Sigal Shalit Manor <sigal.shalit.manor@gmail.com>
2018-08-18 23:42:02 +0300

    commit

--- public/addItem.js
+++ public/addItem.js
@@ -1,2 +1,40 @@
-//remeber to connect this file to index.html!!!!
+var addNewItemsCards = function () {
 
+    var source = $('#item-template').html();
+    var template = Handlebars.compile(source);
+    var cards = [];
+
+    var addNewItem = function (itemName, itemPrice, itemImg) {
+        card = {name: itemName, price: itemPrice, image: itemImg};
+        cards.push.card;
+    };
+
+    var renderNewItem = function () {
+        var newHTML = template({cards});
+        $('.card-container').append(newHTML);
+    };
+ 
+    return {
+        addNewItem: addNewItem,
+        renderNewItem: renderNewItem,
+        cards
+        };
+};
+
+var feature = addNewItemsCards();
+
+feature.renderNewItem();
+
+//--------EVENTS---------
+
+$('.view-add-new-item').on('click', function () {
+    $('.add-item-form').toggle();
+  });
+
+  $('.add-item').on('click' , function () {
+    var $newCardImage = $(this).prev('#item-img').val();
+    var $newCardPrice = $newCardName.prev('#item-price').val();
+    var $newCardName = $formGroupTitle.find('#item-name').val();
+    feature.addNewItem($newCardName, $newCardPrice, $newCardImage).val();
+    feature.renderNewItem();
+  });
